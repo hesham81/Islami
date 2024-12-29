ea89edab9274404d9c9320d0f47578c073b966bf
hesham81 <107004690+hesham81@users.noreply.github.com>
2024-12-29 15:03:24 +0200

    starting with the islami application : -------------------------------------- creating splash screen and his animations

---
+++ lib/core/constants/app_colors.dart
@@ -0,0 +1,8 @@
+import 'dart:ui';
+
+class AppColors {
+  static Color primaryColor = const Color(0xFFE2BE7F);
+  static Color secondaryColor = const Color(0xFF202020);
+  static Color white = const Color(0xFFFFFFFF);
+  static Color titleWhiteColor = const Color(0xFFFEFFE8);
+}
---
+++ lib/core/theme/app_assets.dart
@@ -0,0 +1,13 @@
+class AppAssets {
+  static const String splashBG = "assets/images/background.png";
+  static const String splashRightIMG = "assets/images/Shape-07 1.png";
+  static const String splashLeftIMG = "assets/images/Shape-04.png";
+  static const String splashLogo = "assets/images/Group 36.png";
+  static const String splashBranding = "assets/images/Group 37.png";
+
+  // Icons Section
+  static const String quranICN = "assets/icons/Vector.png";
+  static const String hadethICN = "assets/icons/Page-1.png";
+  static const String radioICN = "assets/icons/lk.png";
+// static const String radioICN = "assets/icons/lk.png";
+}
